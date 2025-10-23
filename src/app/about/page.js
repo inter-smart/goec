5bd24cede62d9a835c5a0f868d8ab03518a2da6f
ajayux <ajay@intersmart.in>
2025-10-23 14:56:59 +0530

    merge with afsal/nikhil

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,7 +10,7 @@ import { fetchFromAPI } from "@/lib/api";
 import Error from "../error";
 
 export default async function AboutPage() {
-  const { data, error } = await fetchFromAPI("about");
+  // const { data, error } = await fetchFromAPI("about");
 
   const heroData = {
     background_media: {
@@ -41,22 +41,22 @@ export default async function AboutPage() {
     ],
   };
 
-  if (error) {
-    return <Error path={"/about"} />;
-  }
+  // if (error) {
+  //   return <Error path={"/about"} />;
+  // }
 
-  const {
-    banner_section,
-    about_section,
-    learn_more_section,
-    mission_vision_section,
-    our_values_section,
-    our_journey_section,
-    meet_team_section,
-    our_associates_section,
-    media_recognition_section,
-    partner_section,
-  } = data;
+  // const {
+  //   banner_section,
+  //   about_section,
+  //   learn_more_section,
+  //   mission_vision_section,
+  //   our_values_section,
+  //   our_journey_section,
+  //   meet_team_section,
+  //   our_associates_section,
+  //   media_recognition_section,
+  //   partner_section,
+  // } = data;
 
   return (
     <>
