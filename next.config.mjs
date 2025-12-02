d19057387d926d4c69320338156a111b8b2bee1f
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-12-02 09:52:28 +0530

    removed headers

--- next.config.mjs
+++ next.config.mjs
@@ -25,32 +25,7 @@ const nextConfig = {
       },
     ],
   },
-  
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: [
-              "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/",
-              "frame-src 'self' https://www.google.com/recaptcha/ https://recaptcha.google.com/recaptcha/",
-              "style-src 'self' 'unsafe-inline' https://www.gstatic.com/recaptcha/",
-              "connect-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/",
-              "img-src 'self' data: https://www.gstatic.com/recaptcha/",
-            ].join("; "),
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-        ],
-      },
-    ];
-  },
-  
+
   webpack(config) {
     config.module.rules.push({
       test: /pdf\.worker(\.min)?\.js$/,
